0c7ae561862a18c1be7c8ad7fe758c81f1ceab96
Bishop b <iyiolablessing98@gmail.com>
2022-12-12 06:24:33 +0100

    done

--- java.js
+++ java.js
@@ -1,3 +1,10 @@
+let conmessage = ""
+let errmessage = ""
+document.getElementById('out').innerHTML = conmessage
+document.getElementById('err').innerHTML = errmessage
+
+
+
 function one(){
     let out = document.getElementById('here').value += "1"
 }
@@ -42,49 +49,503 @@ function nine(){
     
 }
 
+function ten(){
+    let output = document.getElementById('here').value += "10"
+    
+}
+
+function eleven(){
+    let output = document.getElementById('here').value += "11"
+    
+}
+
+function twelve(){
+    let output = document.getElementById('here').value += "12"
+    
+}
+
+function thirteen(){
+    let output = document.getElementById('here').value += "13"
+    
+}
+
+function fourteen(){
+    let output = document.getElementById('here').value += "14"
+    
+}
+
+function fiveteen(){
+    let output = document.getElementById('here').value += "15"
+    
+}
+
+function sixteen(){
+    let output = document.getElementById('here').value += "16"
+    
+}
+
+function seventeen(){
+    let output = document.getElementById('here').value += "17"
+    
+}
+
+function eighteen(){
+    let output = document.getElementById('here').value += "18"
+    
+}
+
+function nineteen(){
+    let output = document.getElementById('here').value += "19"
+    
+}
+
+function twenty(){
+    let output = document.getElementById('here').value += "20"
+    
+}
+
+function twentyone(){
+    let output = document.getElementById('here').value += "21"
+    
+}
+
+function twentytwo(){
+    let output = document.getElementById('here').value += "22"
+    
+}
+
+function twentythree(){
+    let output = document.getElementById('here').value += "23"
+    
+}
+
+function twentyfour(){
+    let output = document.getElementById('here').value += "24"
+    
+}
+
+function twentyfive(){
+    let output = document.getElementById('here').value += "25"
+    
+}
+
+function twentysix(){
+    let output = document.getElementById('here').value += "26"
+    
+}
+
+function twentyseven(){
+    let output = document.getElementById('here').value += "27"
+    
+}
+
+function twentyeight(){
+    let output = document.getElementById('here').value += "28"
+    
+}
+
+function twentynine(){
+    let output = document.getElementById('here').value += "29"
+    
+}
+
+function thirty(){
+    let output = document.getElementById('here').value += "30"
+    
+}
+
+function thirtyone(){
+    let output = document.getElementById('here').value += "31"
+    
+}
+
+function thirtytwo(){
+    let output = document.getElementById('here').value += "32"
+    
+}
+
+function thirtythree(){
+    let output = document.getElementById('here').value += "33"
+    
+}
+
+function tirtyfour(){
+    let output = document.getElementById('here').value += "34"
+    
+}
+
+function thirtyfive(){
+    let output = document.getElementById('here').value += "35"
+    
+}
+
+function thirtysix(){
+    let output = document.getElementById('here').value += "36"
+    
+}
+
+function thirtyseven(){
+    let output = document.getElementById('here').value += "37"
+    
+}
+
+function thirtyeight(){
+    let output = document.getElementById('here').value += "38"
+    
+}
+
+function thirtynine(){
+    let output = document.getElementById('here').value += "39"
+    
+}
+
+function fourty(){
+    let output = document.getElementById('here').value += "40"
+    
+}
+
+function fourtyone(){
+    let output = document.getElementById('here').value += "41"
+    
+}
+
+function fourtytwo(){
+    let output = document.getElementById('here').value += "42"
+    
+}
+
+function fourtythree(){
+    let output = document.getElementById('here').value += "43"
+    
+}
+
+function fourtyfour(){
+    let output = document.getElementById('here').value += "44"
+    
+}
+
+function fourtyfive(){
+    let output = document.getElementById('here').value += "45"
+    
+}
+
+function fourtysix(){
+    let output = document.getElementById('here').value += "46"
+    
+}
+
+function fourtyseven(){
+    let output = document.getElementById('here').value += "47"
+    
+}
+
+function fourtyeight(){
+    let output = document.getElementById('here').value += "48"
+    
+}
+
+function fourtynine(){
+    let output = document.getElementById('here').value += "49"
+    
+}
+
+function fifty(){
+    let output = document.getElementById('here').value += "50"
+    
+}
+
+function fiftyone(){
+    let output = document.getElementById('here').value += "51"
+    
+}
+
+function fiftytwo(){
+    let output = document.getElementById('here').value += "52"
+    
+}
+
+function fiftythree(){
+    let output = document.getElementById('here').value += "53"
+    
+}
+
+function fiftyfour(){
+    let output = document.getElementById('here').value += "54"
+    
+}
+
+function fiftyfive(){
+    let output = document.getElementById('here').value += "55"
+    
+}
+
+function fiftysix(){
+    let output = document.getElementById('here').value += "56"
+    
+}
+
+function fiftyseven(){
+    let output = document.getElementById('here').value += "57"
+    
+}
+
+function fiftyeight(){
+    let output = document.getElementById('here').value += "58"
+    
+}
+
+function fiftynine(){
+    let output = document.getElementById('here').value += "59"
+    
+}
+
+function sixty(){
+    let output = document.getElementById('here').value += "60"
+    
+}
+
+function sixtyone(){
+    let output = document.getElementById('here').value += "61"
+    
+}
+
+function sixtytwo(){
+    let output = document.getElementById('here').value += "62"
+    
+}
+
+function sixtythree(){
+    let output = document.getElementById('here').value += "63"
+    
+}
+
+function sixtyfour(){
+    let output = document.getElementById('here').value += "64"
+    
+}
+
+function sixtyfive(){
+    let output = document.getElementById('here').value += "65"
+    
+}
+
+function sixtysix(){
+    let output = document.getElementById('here').value += "66"
+    
+}
+
+function sixtyseven(){
+    let output = document.getElementById('here').value += "67"
+    
+}
+
+function sixtyeight(){
+    let output = document.getElementById('here').value += "68"
+    
+}
+function sixtynine(){
+    let output = document.getElementById('here').value += "69"
+    
+}
+function sevety(){
+    let output = document.getElementById('here').value += "70"
+}
+function sevetyone(){
+    let output = document.getElementById('here').value += "71"
+}
+function sevetytwo(){
+    let output = document.getElementById('here').value += "72"
+}
+function sevetythree(){
+    let output = document.getElementById('here').value += "73"
+}
+function sevetyfour(){
+    let output = document.getElementById('here').value += "74"
+}
+function sevetyfive(){
+    let output = document.getElementById('here').value += "75"
+}
+function sevetysix(){
+    let output = document.getElementById('here').value += "76"
+}
+function sevetyseven(){
+    let output = document.getElementById('here').value += "77"
+}
+function sevetyeight(){
+    let output = document.getElementById('here').value += "78"
+}
+function sevetynine(){
+    let output = document.getElementById('here').value += "79"
+}
+function eighty(){
+    let output = document.getElementById('here').value += "80"
+}
+function eightyone(){
+    let output = document.getElementById('here').value += "81"
+}
+function eightytwo(){
+    let output = document.getElementById('here').value += "82"
+}
+function eightythree(){
+    let output = document.getElementById('here').value += "83"
+}
+function eightyfour(){
+    let output = document.getElementById('here').value += "84"
+}
+function eightyfive(){
+    let output = document.getElementById('here').value += "85"
+}
+function eightysix(){
+    let output = document.getElementById('here').value += "86"
+}
+function eightyseven(){
+    let output = document.getElementById('here').value += "97"
+}
+function eightyeight(){
+    let output = document.getElementById('here').value += "88"
+}
+function eightynine(){
+    let output = document.getElementById('here').value += "89"
+}
+function ninety(){
+    let output = document.getElementById('here').value += "90"
+}
+function ninetyone(){
+    let output = document.getElementById('here').value += "91"
+}
+function ninetytwo(){
+    let output = document.getElementById('here').value += "92"
+}
+function ninetythree(){
+    let output = document.getElementById('here').value += "93"
+}
+function ninetyfour(){
+    let output = document.getElementById('here').value += "94"
+}
+function ninetyfive(){
+    let output = document.getElementById('here').value += "95"
+}
+function ninetysix(){
+    let output = document.getElementById('here').value += "96"
+}
+function ninetyseven(){
+    let output = document.getElementById('here').value += "97"
+}
+function ninetyeight(){
+    let output = document.getElementById('here').value += "98"
+}
+function ninetynine(){
+    let output = document.getElementById('here').value += "99"
+}
+
+
+
+
+
+
+
+function comma(){
+    let output = document.getElementById('here').value += ", "
+    
+}
 
 function ac(){
     let output = document.getElementById('here').value = ""
- let faith = ""
+    document.getElementById('out').innerHTML = ""
+document.getElementById('err').innerHTML = ""
+//     let conmessage = ""
+// let errmessage = ""
+ 
 
-   
 }
 
 function input(){
-    let comRand = Math.floor(Math.random() * 10 );
+
+    let a = Math.floor(Math.random() * 100 );
+        let b = Math.floor(Math.random() * 100 );
+        let c = Math.floor(Math.random() * 100 );
+        let d = Math.floor(Math.random() * 100 );
+        let e = Math.floor(Math.random() * 100 );
+        let f = Math.floor(Math.random() * 100 );
+
+        const randNum = [a, b, c, d, e, f];
+        console.log(randNum)
+        let randy = randNum.toString()
+    
     let usernum = document.getElementById('here').value;
+    let User = usernum.split(",")
+    console.log(User);
+    const check = [];
+    let conmessage = ""
+    let errmessage = ""
+    
+    let i = 0;
 
-    let mm = usernum.toString()
-    let mn = mm.length
+    for(let value of randNum){
+        if(randNum.includes(User[i]) == true && randNum.includes(User[i])&& randNum.includes(User[i]) && User.length == 3 || User.length == 2){
+            check[i] = User[i];
+            if(User.length == 3 && check.length == 3){
+                console.log(User)
+                conmessage = "You Win</br>" + `Your predicted numbers are ${usernum} and the random numbers are ${randy}`
+            }
+            else if(User.length == 2 && check.length == 2){
+                console.log(User)
+                conmessage = "You Win</br>" + `Your predicted numbers are ${usernum} and the random numbers are ${randy}`
+            }
+            else{
+                conmessage = 'Enter 2 or 3 numbers separated with a comma to play'
+            }
 
-    if (mn > 1){
-        faith = "Input a number between 1 and 9"
-        console.log("Input a number between 1 and 9")
+        }
+        else if(User.length > 3 || User.length < 2 || User.length == []){
+            errmessage = "Enter 2 or 3 numbers separated with a comma to play"
+            conmessage = ""
+        }
+        else{
+            conmessage = "You Lose</br>" + ` Try Again your predicted numbers are ${usernum} and the random numbers are ${randy}`
+        }
+        
+        i++
+        document.getElementById('out').innerHTML = conmessage
+        document.getElementById('err').innerHTML = errmessage
     }
-
-    if(mm = ""){
-        console.log('empty')
-    }
-
-    if(comRand == usernum && mn == 1){
-     faith = ` You Win your predicted number is ${usernum} and random number is ${comRand}`;
-     }
-
-    if(comRand != usernum && mn == 1){
-        faith = `You Lose your predicted number is ${usernum} and random number is ${comRand}`
-    }
-
-    else{
-        faith = `Input a number between 1 and 9`
-    }
-    document.getElementById('put').innerHTML=faith;
+    
 }
 
+   
 
 
-let number = 3
-console.log(number)
-let num = number.toString()
-console.log(num.length)
 
 
+
+
+    // let mn = mm.length
+
+    // let User = [mm]
+    // console.log(mm)
+    // const B = [1,2,3,5]
+    // console.log(B.length)
+
+
+    // if (mn > 1){
+    //     faith = "Input a number between 1 and 9"
+    //     console.log("Input a number between 1 and 9")
+    // }
+
+    // if(mm = ""){
+    //     console.log('empty')
+    // }
+
+    // if(comRand == usernum && mn == 1){
+    //  faith = ` You Win your predicted number is ${usernum} and random number is ${comRand}`;
+    //  }
+
+    // if(comRand != usernum && mn == 1){
+    //     faith = `You Lose your predicted number is ${usernum} and random number is ${comRand}`
+    // }
+
+    // else{
+    //     faith = `Input a number between 1 and 9`
+    // }
+    // document.getElementById('put').innerHTML=faith;
+// 
